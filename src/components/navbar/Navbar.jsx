394b339d4b32e3ee0585c92820ebb8b27bc2c8a6
Name <damiros.20002@gmail.com>
2023-05-24 00:02:27 +0300

    залил

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 // import ManageSearchIcon from '@mui/icons-material/ManageSearch';
 import LogoutIcon from '@mui/icons-material/Logout';
+import { Image } from 'react-bootstrap';
 import { useNavigate } from "react-router-dom";
 import { Link } from 'react-router-dom'
 import "./navbar.scss"
+import logo from './logo-no-background.png';
 
 const Navbar = ({setIsAuth}) => {
 
